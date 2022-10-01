890944a36ccbdc7eecba585c5e53178fc3a1ecac
andrewlfauth <andrewlfauth@gmail.com>
2022-09-30 19:59:00 -0700

    Add: navbar

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,8 +1,16 @@
+import { Link } from '@remix-run/react'
+
 function Navbar() {
   return (
-    <div>
-      <nav className='flex items-center justify-end'>
-        <button></button>
+    <div className='bg-blue-800 py-2'>
+      <nav className='flex items-center justify-between max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
+        <Link to='/'>LOGO</Link>
+
+        <div>
+          <Link className='bg-gray-100 px-2 py-1 rounded' to='/auth'>
+            Sign In
+          </Link>
+        </div>
       </nav>
     </div>
   )
