a944eb7f8b1dece116b22aad2a53f3f5f664cfe7
andrewlfauth <andrewlfauth@gmail.com>
2022-09-30 19:58:03 -0700

    Add: logout func

---
+++ app/routes/logout.jsx
@@ -0,0 +1,13 @@
+import { redirect } from '@remix-run/node'
+
+import { getSession, destroySession } from '~/sessions'
+
+export async function loader({ request }) {
+  const session = await getSession(request.headers.get('Cookie'))
+
+  return redirect('/', {
+    headers: {
+      'Set-Cookie': await destroySession(session),
+    },
+  })
+}
