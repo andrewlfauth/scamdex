43b13bfaf8d9f1eaf450eede4d70019580f67b3b
andrewlfauth <andrewlfauth@gmail.com>
2022-10-01 12:40:23 -0700

    Add: sign in/out button

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,15 +1,23 @@
-import { Link } from '@remix-run/react'
+import { Link, useLocation } from '@remix-run/react'
 
 function Navbar() {
+  const path = useLocation().pathname
+
   return (
-    <div className='bg-blue-800 py-2'>
-      <nav className='flex items-center justify-between max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
+    <div className='py-2 bg-blue-800'>
+      <nav className='flex items-center justify-between px-4 mx-auto max-w-7xl sm:px-6 lg:px-8'>
         <Link to='/'>LOGO</Link>
 
         <div>
-          <Link className='bg-gray-100 px-2 py-1 rounded' to='/auth'>
-            Sign In
-          </Link>
+          {path === '/' || path === '/auth' ? (
+            <Link className='px-2 py-1 bg-gray-100 rounded' to='/auth'>
+              Sign In
+            </Link>
+          ) : (
+            <Link className='px-2 py-1 bg-gray-100 rounded' to='/logout'>
+              Sign Out
+            </Link>
+          )}
         </div>
       </nav>
     </div>
