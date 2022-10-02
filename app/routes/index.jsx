f425f1b092343435f7a9c53a82c2d123a92d8d37
andrewlfauth <andrewlfauth@gmail.com>
2022-10-01 19:22:48 -0700

    Styling

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,15 +1,28 @@
 import { Link } from '@remix-run/react'
+import { redirect } from '@remix-run/node'
+
+import { getSession } from '../sessions'
+
+export async function loader({ request }) {
+  const session = await getSession(request.headers.get('Cookie'))
+
+  if (session.has('userId')) {
+    return redirect('/call-center')
+  }
+
+  return null
+}
 
 function index() {
   return (
-    <div className='px-4 sm:px-6 lg:px-8 mx-auto max-w-7xl py-20'>
+    <div className='px-4 py-20 mx-auto sm:px-6 lg:px-8 max-w-7xl'>
       <div className='max-w-xl'>
         <h1 className='text-6xl font-semibold tracking-tight'>
           Keep track of your ongoing scambaits
         </h1>
         <Link
           to='/auth'
-          className='bg-fuchsia-500 text-gray-100 text-xl rounded font-semibold py-3 w-44 text-center block mt-10'
+          className='block py-3 mt-10 text-xl font-semibold text-center text-gray-100 rounded bg-fuchsia-500 w-44'
         >
           Get started
         </Link>
