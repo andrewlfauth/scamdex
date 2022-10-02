f9710bbcb1c71dfd7a279f1ffb0c434e7e9f0ae2
andrewlfauth <andrewlfauth@gmail.com>
2022-10-02 12:22:58 -0700

    Styling

--- app/components/dashboard/CallCenterTitle.jsx
+++ app/components/dashboard/CallCenterTitle.jsx
@@ -1,11 +1,16 @@
 import { VscEdit } from 'react-icons/vsc'
 import { useState, useEffect, useRef } from 'react'
-import { Form } from '@remix-run/react'
 
 function CallCenterTitle() {
   const [title, setTitle] = useState()
   const [showForm, setShowForm] = useState(false)
   const inputRef = useRef()
+
+  const closeOnEsc = (e) => {
+    if (e.key === 'Escape') {
+      setShowForm(false)
+    }
+  }
 
   const saveName = () => {
     setTitle(inputRef.current.value)
@@ -22,16 +27,25 @@ function CallCenterTitle() {
   }, [])
 
   useEffect(() => {
+    if (showForm) {
+      document.addEventListener('keydown', closeOnEsc)
+      return () => document.removeEventListener('keydown', closeOnEsc)
+    }
+  }, [showForm])
+
+  useEffect(() => {
     window.localStorage.setItem('CALL_CENTER_TITLE', title)
   }, [title])
 
   return title ? (
     <>
-      <h1 className='text-2xl font-semibold text-type-white relative group cursor-pointer flex'>
+      <h1 className='text-2xl font-semibold text-type-white relative group cursor-pointer flex pr-4 pt-2 -mt-2'>
         {title}
         <VscEdit
-          className=' text-type-secondary rounded-md cursor-pointer group-hover:block -mt-3 ml-1 hidden hover:text-accent-blue'
-          onClick={() => setShowForm(true)}
+          className={`${
+            showForm ? 'text-accent-blue' : 'hidden group-hover:block'
+          } text-type-secondary rounded-md cursor-pointer -mt-3 ml-1  text-[18px] `}
+          onClick={() => setShowForm(!showForm)}
         />
       </h1>
       <p className='text-sm text-type-secondary mt-1'>
@@ -39,11 +53,11 @@ function CallCenterTitle() {
       </p>
 
       {showForm && (
-        <div className='bg-secondary mt-2 absolute p-4 rounded'>
+        <div className='bg-secondary mt-2 absolute px-4 py-3 rounded'>
           <input
             ref={inputRef}
             type='text'
-            className='bg-type-secondary rounded px-2 py-1 text-type-white outline-accent-blue'
+            className='border-2 border-type-secondary rounded bg-transparent px-2 py-1 text-type-white outline-accent-blue ring-0'
           />
           <button
             className='text-type-white ml-4 hover:text-accent-blue'
