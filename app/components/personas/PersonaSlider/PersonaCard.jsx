f119648a10e07c3d7701c71e37c4466eeced7b10
andrewlfauth <andrewlfauth@gmail.com>
2022-10-02 18:28:51 -0700

    Style: creating slider

--- app/components/personas/PersonaSlider/PersonaCard.jsx
+++ app/components/personas/PersonaSlider/PersonaCard.jsx
@@ -1,19 +1,13 @@
-import React from 'react'
-
-function PersonaCard() {
-  let p = {
-    name: 'Edna',
-    age: '83',
-    memoji:
-      'https://res.cloudinary.com/dpnkrz8c8/image/upload/v1664651962/classroom/Female_Memojis_dkwxqr.png',
-    bio: "Endna's hobbies include mayonaise and purchasing bathtubs.",
-  }
-
+function PersonaCard({ persona, className }) {
   return (
-    <div className='flex flex-col items-center px-4 py-6 rounded-md mx-auto bg-secondary w-[150px]'>
-      <img src={p.memoji} alt='' className='w-20' />
-      <h4 className='mb-4 -mt-3 font-semibold text-type-primary'>{p.name}</h4>
-      <p className='text-xs text-center text-type-secondary'>{p.bio}</p>
+    <div
+      className={`${className} flex flex-col items-center px-4 py-6 rounded-md mx-auto bg-secondary select-none max-w[150px] min-w-[150px] h-[200px]`}
+    >
+      <img src={persona.memoji} alt='' className='w-20' />
+      <h4 className='mb-4 -mt-3 font-semibold text-type-primary'>
+        {persona.name}
+      </h4>
+      <p className='text-xs text-center text-type-secondary'>{persona.bio}</p>
     </div>
   )
 }
--- app/components/personas/PersonaSlider/index.jsx
+++ app/components/personas/PersonaSlider/index.jsx
@@ -1,18 +1,60 @@
+import { useState, useRef, useEffect } from 'react'
 import { AiOutlineRight, AiOutlineLeft } from 'react-icons/ai'
 
 import PersonaCard from './PersonaCard'
 
-function index() {
+function Index() {
+  const [idx, setIdx] = useState(0)
+  const sliderRef = useRef()
+
+  const handleNext = () => {
+    setIdx((idx + 1) % p.length)
+  }
+
+  const handlePrev = () => {
+    setIdx((idx + p.length - 1) % p.length)
+  }
+
+  let p = [
+    {
+      name: 'Edna',
+      age: '83',
+      memoji:
+        'https://res.cloudinary.com/dpnkrz8c8/image/upload/v1664651962/classroom/Female_Memojis_dkwxqr.png',
+      bio: "Endna's hobbies include mayonaise and purchasing bathtubs.",
+    },
+    {
+      name: 'Bob',
+      age: '3',
+      memoji:
+        'https://res.cloudinary.com/dpnkrz8c8/image/upload/v1664651962/classroom/Female_Memojis_dkwxqr.png',
+      bio: 'Fun stuff and stuff.',
+    },
+    {
+      name: 'Jane',
+      age: '33',
+      memoji:
+        'https://res.cloudinary.com/dpnkrz8c8/image/upload/v1664651962/classroom/Female_Memojis_dkwxqr.png',
+      bio: 'Everyting i sokay i think.',
+    },
+  ]
+
   return (
     <div className='px-10 py-3 border border-red-100 rounded-md'>
       <h2 className='mb-4 text-type-primary'>Your Personas</h2>
 
       <div className='flex items-center'>
-        <button className='group'>
+        <button className='group p-2' onClick={handlePrev}>
           <AiOutlineLeft className='text-type-secondary group-hover:text-type-primary' />
         </button>
-        <PersonaCard />
-        <button className='group'>
+
+        <div ref={sliderRef} className='flex overflow-hidden w-[150px]'>
+          {p.map((p, i) => (
+            <PersonaCard key={p} persona={p} />
+          ))}
+        </div>
+
+        <button className='group p-2' onClick={handleNext}>
           <AiOutlineRight className='font-bold text-type-secondary group-hover:text-type-primary' />
         </button>
       </div>
@@ -20,4 +62,4 @@ function index() {
   )
 }
 
-export default index
+export default Index
