644a0fae988e9290f0045a18eb31ca16c4b3c9a5
andrewlfauth <andrewlfauth@gmail.com>
2022-10-03 19:01:37 -0700

    Style: p-4

--- app/components/personas/personaMaker/index.jsx
+++ app/components/personas/personaMaker/index.jsx
@@ -7,8 +7,8 @@ import BioInput from './BioInput'
 
 function PersonaMaker() {
   return (
-    <div className='bg-secondary w-[340px] rounded-md p-3'>
-      <h2 className='text-type-primary mb-4'>Persona Factory</h2>
+    <div className='bg-secondary w-[340px] rounded-md p-4'>
+      <h2 className='mb-4 text-type-primary'>Persona Factory</h2>
       <Form method='post' className='flex flex-col'>
         <div className='flex space-x-6'>
           <NameInput />
@@ -18,7 +18,7 @@ function PersonaMaker() {
         <BioInput />
         <button
           type='submit'
-          className='bg-accent-purple py-2 rounded-md text-secondary text-sm px-6 mt-6 hover:ring-2 ring-offset-2 ring-offset-secondary duration-200 ring-accent-purple w-fit self-end font-semibold'
+          className='self-end px-6 py-2 mt-6 text-sm font-semibold duration-200 rounded-md bg-accent-purple text-secondary hover:ring-2 ring-offset-2 ring-offset-secondary ring-accent-purple w-fit'
         >
           Create
         </button>
