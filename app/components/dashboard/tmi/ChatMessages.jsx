3e1634db50ad6e3941a0579ee389283b37800da2
andrewlfauth <andrewlfauth@gmail.com>
2022-10-06 22:39:33 -0700

    Add: pauseOnP func

--- app/components/dashboard/tmi/ChatMessages.jsx
+++ app/components/dashboard/tmi/ChatMessages.jsx
@@ -1,19 +1,55 @@
-import { useRef } from 'react'
+import { useEffect, useRef } from 'react'
 import { useAtom } from 'jotai'
 
 import Message from './Message'
-import { chatSettingsAtom } from './index'
+import { chatSettingsStorageAtom } from './index'
 
-function ChatMessages({ chatMessages, play, pause }) {
-  const [settings, setSettings] = useAtom(chatSettingsAtom)
+function ChatMessages({ state, chatMessages, controls }) {
   const chatRef = useRef()
+  const [settings, setSettings] = useAtom(chatSettingsStorageAtom)
+
+  // pauseOnP listeners
+  useEffect(() => {
+    const handleKeyDown = (e) => {
+      if (e.key === 'p') controls.pause()
+    }
+
+    const handleKeyUp = (e) => {
+      if (e.key === 'p') controls.play()
+    }
+
+    if (settings.pauseOnP) {
+      if (state !== 'stopped') {
+        document.addEventListener('keydown', handleKeyDown)
+        document.addEventListener('keyup', handleKeyUp)
+      } else {
+        document.removeEventListener('keydown', handleKeyDown)
+        document.removeEventListener('keyup', handleKeyUp)
+      }
+    }
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown)
+      document.removeEventListener('keyup', handleKeyUp)
+    }
+  }, [settings, state, controls])
+
+  const handleMouseEnter = () => {
+    if (state === 'playing') {
+      controls.pause()
+    }
+  }
+
+  const handleMouseLeave = () => {
+    controls.play()
+  }
 
   return (
     <div
       ref={chatRef}
+      // onMouseEnter={handleMouseEnter}
+      // onMouseLeave={handleMouseLeave}
       className='inline h-full p-3 chat'
-      onMouseEnter={settings.pauseOnHover ? pause : null}
-      onMouseLeave={settings.pauseOnHover ? play : null}
     >
       {chatMessages.map((m, i) => (
         <Message key={i} item={m} />
