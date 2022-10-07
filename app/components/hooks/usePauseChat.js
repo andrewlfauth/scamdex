9ccd1405c548c08ca58b7aa4761e986c3c82c6c7
andrewlfauth <andrewlfauth@gmail.com>
2022-10-06 22:55:36 -0700

    Add: usePauseChat hook

---
+++ app/components/hooks/usePauseChat.js
@@ -0,0 +1,42 @@
+import { useEffect } from 'react'
+
+export default function usePauseChat(state, controls, settings) {
+  const handleMouseEnter = () => {
+    if (state === 'playing') {
+      controls.pause()
+    }
+  }
+
+  const handleMouseLeave = () => {
+    if (state !== 'stopped') {
+      controls.play()
+    }
+  }
+
+  useEffect(() => {
+    const handleKeyDown = (e) => {
+      if (e.key === 'p') controls.pause()
+    }
+
+    const handleKeyUp = (e) => {
+      if (e.key === 'p') controls.play()
+    }
+
+    if (settings.pauseOnP) {
+      if (state !== 'stopped') {
+        document.addEventListener('keydown', handleKeyDown)
+        document.addEventListener('keyup', handleKeyUp)
+      } else {
+        document.removeEventListener('keydown', handleKeyDown)
+        document.removeEventListener('keyup', handleKeyUp)
+      }
+    }
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown)
+      document.removeEventListener('keyup', handleKeyUp)
+    }
+  }, [settings, state, controls])
+
+  return { handleMouseLeave, handleMouseEnter }
+}
