3e74d5fed218ae233c844366065d72ba973c721a
andrewlfauth <andrewlfauth@gmail.com>
2022-10-14 23:06:38 -0700

    TODO: add userId to document

---
+++ app/utils/personas.server.js
@@ -0,0 +1,36 @@
+import { redirect } from '@remix-run/node'
+import cloudinary from 'cloudinary'
+
+import { getSession } from '../sessions'
+import Personas from '../models/Personas'
+import Users from '../models/Users'
+
+export async function createPersona(request, values) {
+  // const session = await getSession(request.headers.get('Cookie'))
+  // if (!session.has('userId')) {
+  //   return redirect('/auth')
+  // }
+  // const user = await Users.findOne({
+  //   id: session.userId,
+  // })
+  // if (!user) {
+  //   return redirect('/auth')
+  // }
+  // const userId = user._id.toString()
+
+  await cloudinary.v2.uploader
+    .upload(values.audio, {
+      public_id: 'bait-tracker/persona/test',
+      resource_type: 'auto',
+    })
+    .then((res) => (values.audio = res.url))
+    .catch((e) => console.error(e))
+
+  await Personas.create({
+    name: values.name,
+    age: values.age,
+    memoji: values.memoji,
+    audio: values.audio,
+    bio: values.bio,
+  })
+}
