fe7cec2d786360f29052b7bed73a4494f34a7fc4
andrewlfauth <andrewlfauth@gmail.com>
2022-10-16 13:28:42 -0700

    STYLE: SavedCallCard

--- app/components/calls/SavedCallCard.jsx
+++ app/components/calls/SavedCallCard.jsx
@@ -5,26 +5,33 @@ function SavedCallCard({ call }) {
   let isActive = activeCall && activeCall._id == call._id
 
   return (
-    <div className='relative p-4 rounded-md bg-secondary'>
-      <div className='flex items-end justify-between mb-4'>
-        <h3 className='font-semibold text-type-secondary whitespace-nowrap'>
+    <div className='p-4 flex justify-between rounded-md bg-secondary space-x-10'>
+      <div>
+        <h3 className='font-semibold text-type-primary tracking-tight whitespace-nowrap text-sm'>
           {call.name}
         </h3>
+        <span className='text-2xl text-accent-blue font-semibold tracking-tight ml-2 py-2 block'>
+          1.2 hrs
+        </span>
+        <span className='text-xs block font-semibold text-type-secondary'>
+          {call.scamCompany}
+        </span>
+      </div>
+      <div className='flex flex-col justify-between items-end'>
+        <img
+          src={call.persona.memoji}
+          alt=''
+          className='relative -top-2 -right-2'
+        />
         <button
           className={`${
             isActive ? 'bg-accent-red' : 'bg-accent-blue'
-          } w-24 py-1 ml-4 font-bold duration-100 rounded text-secondary hover:bg-opacity-80`}
+          } w-16 text-xs py-1 font-bold duration-100 rounded  hover:bg-opacity-80 text-secondary`}
           onClick={isActive ? () => setActiveCall() : () => setActiveCall(call)}
         >
-          {isActive ? 'Close Call' : 'Open Call'}
+          {isActive ? 'Close' : 'Open'}
         </button>
       </div>
-      <span className='block mb-1 text-sm font-semibold text-type-secondary'>
-        Fake {call.scamCompany}
-      </span>
-      <span className='block font-semibold text-type-secondary'>
-        {call.scamNumber}
-      </span>
     </div>
   )
 }
